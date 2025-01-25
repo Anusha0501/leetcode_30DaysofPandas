42fbd923831ba884809aaf2c7a591ced144c433d
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-25 19:45:39 +0530

    Create 1683.py

---
+++ 1683.py
@@ -0,0 +1,8 @@
+#1683. Invalid Tweets
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    invalid = tweets[tweets['content'].str.len() > 15]
+    return invalid[['tweet_id']]
+    
