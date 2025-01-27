1fffc292e60e848963f86abb380f1b2464f115dc
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-27 23:21:00 +0530

    Create 596.py

---
+++ 596.py
@@ -0,0 +1,7 @@
+#596. Classes More Than 5 Students
+
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    df = courses.groupby(by='class').count().reset_index()
+    return df[df['student']>=5][['class']]
