63b1cf8f5d973fee8b2f2fdabf35f7b95ca7d87e
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-31 21:47:00 +0530

    Create 1484.py

---
+++ 1484.py
@@ -0,0 +1,13 @@
+#1484. Group Sold Products By The Date
+
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    
+    df = activities.groupby('sell_date').agg(
+        num_sold = ('product','nunique'),
+        products = ('product',  lambda x: ','.join(sorted(set(x))))  # Sort and join unique product names
+    ).reset_index()
+    
+    return df
+
